6f74ef1a81e307ee8a812c260d5b52ece946ac12
Leonardo Heredia <leonardo.heredia@hp.com>
2022-08-24 14:45:03 -0300

    Documentação

--- src/main/heap/ExemploMaxHeap.java
+++ src/main/heap/ExemploMaxHeap.java
@@ -18,6 +18,22 @@ public class ExemploMaxHeap {
         //Testar essa mesma ordem visualizando passo a passo em http://btv.melezinek.cz/binary-heap.html
 
 
+        MaxHeap heap2 = new MaxHeap(new int[]{0,4,10,3,5,1, 99, 65, 32});
+        heap2.printArray();
+
+        heap2.print();
+
+        heap2.sort();
+        heap2.printArray();
+
+
+        MaxHeap heap3 = new MaxHeap(new int[]{0,4, 10, 3, 5, 1});
+        heap3.printArray();
+        heap3.sort();
+
+        heap3.printArray();
+
+
     }
 
 }
